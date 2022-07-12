7bf40728e7ceef1ae4a0e9b5d7f850afb12fdb88
mopore <jni@mopore.org>
2022-07-12 16:17:51 +0200

    Learning the state of the current MQTT connection.

--- src/shared/mqtt/MqttServerConnection.ts
+++ src/shared/mqtt/MqttServerConnection.ts
@@ -5,20 +5,47 @@ export class MqttServerConnection {
 
 	private _client: Client;
 
+	private _connected = false;
+
 	constructor(mqttServerUrl: string){
 		console.log(`Connecting to MQTT server via "${mqttServerUrl}"...`);
+		
 		this._client = MQTT.connect(mqttServerUrl, {
 			connectTimeout: 3000
 		});
-
+		
 		this._client.on("connect", () => {
 			console.log("Connected to MQTT server");
-		});	
+			this._connected = true;
+		});
+
+		this._client.on("disconnect", () => {
+			console.error("Lost the connection to MQTT server...");
+			this._connected = false;
+		})
 
 		this._client.setMaxListeners(15);
 	}
 
+
+	get connected(): boolean {
+		return this.connected;
+	}
+
+
+	attemptReconnect(): void{
+		console.info("Attempting to reconnect...");
+		this._client.reconnect();
+	}
+
+
 	publish(topic: string, message: string): void{
+		if (! this._connected) {
+			const errorMessage = `Error trying to publishing without connection!`;
+			console.error(errorMessage);
+			console.trace();
+			throw new Error(errorMessage);
+		}
 		this._client.publish(topic, message);
 	}
 
